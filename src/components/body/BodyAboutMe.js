be8e8763a903324e0129ade83037dbced6129796
CristianATZ <cristiantorreszvl@gmail.com>
2024-08-15 18:40:24 -0600

    Implementar seccion Acerca de mi en Body

---
+++ src/components/body/BodyAboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BodyAboutMe() {
+    return (
+        <body className='bg-background-light h-screen w-full'>
+            <div className='p-5 w-full flex justify-center'>
+                <p className='text-surface-variant-dark/25 text-6xl font-black'>about me</p>
+            </div>
+
+        </body>
+    );
+}
+
+export default BodyAboutMe;
